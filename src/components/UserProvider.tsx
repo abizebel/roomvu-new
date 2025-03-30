d300fb2ce1b5e647d1489f826a04c2b20aeed4ab
abbas <abizebel@gmail.com>
2025-03-30 10:34:14 -0500

    Remove Extra files

--- src/components/UserProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { UserProvider as Auth0UserProvider } from '@auth0/nextjs-auth0/client';
-import { ReactNode } from 'react';
-
-export default function UserProvider({ children }: { children: ReactNode }) {
-  return <Auth0UserProvider>{children}</Auth0UserProvider>;
-} 
